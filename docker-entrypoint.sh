30d5f01ec8eb4a34bd54034383183ce5aa90e285
Dan Allen <dan.j.allen@gmail.com>
2021-04-19 16:03:25 -0600

    resolves #20 verify entrypoint is executable before accepting it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # prepend antora if command is not detected
-if [ $# -eq 0 ] || [ "${1:0:1}" == '-' ] || [ -z `command -v "$1" || echo -n` ]; then
+if [ $# -eq 0 ] || [ "${1:0:1}" == '-' ] || [ -z "$(command -v "$1")" ] || [ -d "$(command -v "$1")" ] || [ ! -x "$(command -v "$1")" ]; then
   set -- antora "$@"
 fi
 
